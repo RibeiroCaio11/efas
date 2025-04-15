81cf72d9ff1f2c2388dcf10803b042cf49b74130
Caio Ribeiro <colribeiro05@gmail.com>
2025-04-14 22:30:52 -0300

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ class Config:
         
     def _set_environment_variables(self):
         """Define as variáveis de ambiente com as chaves de API"""
-        os.environ["VT_API_KEY"] = "465392e355f68039e47c72ce731b82fbc4459d3903813530cab0214af927f4f3"
-        os.environ["ABUSE_IPDB_KEY"] = "356676efec9ac05ed7bbde67d84325da5009fab0267ada0ef296a2acafe728420d3f171ab37dd54d"
-        os.environ["TELEGRAM_BOT_TOKEN"] = "7850300754:AAFlB_0ugzCtHnLmqar6l5a5VnRxziGNaxo"
-        os.environ["TELEGRAM_CHAT_ID"] = "7111234268"
-        os.environ["DISCORD_WEBHOOK"] = "https://discord.com/api/webhooks/1361386545512452207/t4m9UUXLZo5pzf6dbydEKGX6cfu_KrlN1puTlZ7j3IlEvgqXIzS5-1UnNV3EJ8oOTMhQ"
+        os.environ["VT_API_KEY"] = 
+        os.environ["ABUSE_IPDB_KEY"] = 
+        os.environ["TELEGRAM_BOT_TOKEN"] = 
+        os.environ["TELEGRAM_CHAT_ID"] = ""
+        os.environ["DISCORD_WEBHOOK"] = ""
         # Usando a chave de API fornecida pelo usuário
-        os.environ["OPENAI_API_KEY"] = "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"
+        os.environ["OPENAI_API_KEY"] = ""
     
     def _load_api_keys(self):
         """Carrega as chaves de API das variáveis de ambiente"""
